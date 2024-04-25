84e401fa887e413efb106a0455b3071de17f925c
MicaVazquez <mikaela.vzqz@gmail.com>
2024-04-25 10:29:50 -0300

    sprint 1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./components/login/login.component').then(
+        (m) => m.LoginComponent
+      ),
+  },
+  {
+    path: 'presentacion',
+    loadComponent: () =>
+      import('./components/presentacion/presentacion.component').then(
+        (m) => m.PresentacionComponent
+      ),
+  },
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./components/home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./components/login/login.component').then(
+        (m) => m.LoginComponent
+      ),
+  },
+];
---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,16 @@
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
+})
+export class LoginComponent {
+  router2 = inject(Router);
+  login() {
+    this.router2.navigateByUrl('home');
+  }
+}
